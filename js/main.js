c51bf0deedcd8d492595c2fc035dbc254442510e
Michael <Michael@zingdesignsimac.fritz.box>
2014-11-05 09:47:01 +1300

    New navigation
    yay!

--- js/main.js
+++ js/main.js
@@ -1,2 +1,3 @@
 $('.full-height').css({'height':($(window).height())+'px'});
 $('#header-content').css({'margin-top':((($('#home-header').height())/2)-(($('#header-content').height())/2))+'px'});
+$('.navigation--draw label').click(function(){$('#navigation--toggle').attr("checked", false);});

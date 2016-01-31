7c828498578f3509387819bfe971cd18f3e44b66
Michael <macorlett@gmail.com>
2016-02-01 11:34:15 +1300

    New version
    New site/blog version!

---
+++ _site/js/main.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+
+resizeend();
+console.log($(window).height());
+
+wrapTitleCharacters();
+
+});
+
+var rtime;
+var timeout = false;
+var delta = 200;
+$(window).resize(function() {
+  rtime = new Date();
+  if (timeout === false) {
+    timeout = true;
+    setTimeout(resizeend, delta);
+  }
+});
+
+function resizeend() {
+  if (new Date() - rtime < delta) {
+    setTimeout(resizeend, delta);
+  } else {
+    timeout = false;
+    if($(window).width()>799){
+      $('.js--full-height').css({height:$(window).height()+"px"});
+    }else{
+      $('.js--full-height').css({height:"auto"});
+    }
+  }               
+}
+function wrapTitleCharacters(){
+  $('.post h1').each(function(){
+    var words = $(this).html().split(' ');
+    var count = words.length;
+    words.splice(count-2,0, '<span>');
+    words.push('</span>');
+    str=words.join(" ");
+    $(this).html(str);
+  });
+}

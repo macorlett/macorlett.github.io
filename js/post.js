097466f3f2f96380f4a4809ba9ed892bed16c1c4
Michael <macorlett@gmail.com>
2016-03-09 13:16:42 +1300

    transitions add to article wrapper

--- js/post.js
+++ js/post.js
@@ -1,9 +1,12 @@
 $(document).ready(function(){
 	$('.js--article--next').mouseenter(function(){
 		$('.article--next').addClass('article--next--active');
+		move=$('article--next').width();
+		$('.article--wrapper').css({transform:'translateX(-'+move+'px)'});
 	});
 	$('.js--article--next').mouseleave(function(){
 		$('.article--next').removeClass('article--next--active');
+		$('.article--wrapper').css({transform:'translateX(0)'});
 	});
 	$('.js--article--prev').mouseenter(function(){
 		$('.article--prev').addClass('article--prev--active');

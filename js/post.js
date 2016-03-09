ce12bcdd864d1a1fae597a34b3a97bfb059864c9
Michael <macorlett@gmail.com>
2016-03-09 13:24:51 +1300

    refined push script

--- js/post.js
+++ js/post.js
@@ -2,16 +2,27 @@ $(document).ready(function(){
 	$('.js--article--next').mouseenter(function(){
 		$('.article--next').addClass('article--next--active');
 		move=$('.article--next').width();
-		$('.article--wrapper').css({transform:'translateX(-'+move+'px)'});
+		pushElement($('.article--wrapper'),-move);
+		pushElement($('.article--prev'),-move);
 	});
 	$('.js--article--next').mouseleave(function(){
 		$('.article--next').removeClass('article--next--active');
-		$('.article--wrapper').css({transform:'translateX(0)'});
+		pushElement($('.article--wrapper'),0);
+		pushElement($('.article--prev'),0);
 	});
 	$('.js--article--prev').mouseenter(function(){
 		$('.article--prev').addClass('article--prev--active');
+		move=$('.article--prev').width();
+		pushElement($('.article--wrapper'),move);
+		pushElement($('.article--prev'),move);
 	});
 	$('.js--article--prev').mouseleave(function(){
 		$('.article--prev').removeClass('article--prev--active');
+		pushElement($('.article--wrapper'),0);
+		pushElement($('.article--next'),0);
 	});
 });
+
+function pushElement(obj,num){
+	obj.css({transform:'translateX('+num+'px)'});
+}
